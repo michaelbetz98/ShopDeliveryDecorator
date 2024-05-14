3dbd9a0df3350c7c0f66e0c58579a41f2a06ed3d
michaelbetz <michael.schabetz@stud.tecnicosuperiorekennedy.it>
2024-05-14 12:16:46 +0200

    Edit input

--- ShopDelivery/Program.cs
+++ ShopDelivery/Program.cs
@@ -5,16 +5,26 @@
         static void Main(string[] args)
         {
             Delivery delivery = new DeliveryInStore();
-            Console.WriteLine($"{delivery.Chosen()} price: {delivery.Price()}" );
-
-            var deliverStandard = new DeliveryStandard(delivery);
-            Console.WriteLine($"{deliverStandard.Chosen()} price: {deliverStandard.Price()}");
-
-            var deliveryExpress = new DeliveryExpress(deliverStandard);
-            Console.WriteLine($"{deliveryExpress.Chosen()} price: {deliveryExpress.Price()}");
-
-            var deliveryExpressGiftBox = new DeliveryGiftBox(deliveryExpress);
-            Console.WriteLine($"{deliveryExpressGiftBox.Chosen()} price: {deliveryExpressGiftBox.Price()}");
+            Console.WriteLine("Do you want it to be delivered? (yes/no)");
+            string deliver = Console.ReadLine();
+            if(deliver == "yes")
+            {
+                delivery = new DeliveryStandard(delivery);
+            }
+            Console.WriteLine("Do you want to have and express delivery? (yes/no)");
+            string express = Console.ReadLine();
+            if (express == "yes")
+            {
+                delivery = new DeliveryExpress(delivery);
+            }
+            Console.WriteLine("Do you want it ina gift box? (yes/no)");
+            string gift = Console.ReadLine();
+            if (gift == "yes")
+            {
+                delivery = new DeliveryGiftBox(delivery);
+            }
+            Console.WriteLine($"{delivery.Chosen()} price: {delivery.Price()}");
+            Console.ReadLine();
         }
     }
 }
