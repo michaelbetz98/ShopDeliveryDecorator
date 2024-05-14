bf80b1480c570a5082c2e1fe3607bc92c3f7b40e
michaelbetz <michael.schabetz@stud.tecnicosuperiorekennedy.it>
2024-05-14 10:37:58 +0200

    Add project files.

---
+++ ShopDelivery/Delivery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDelivery
+{
+    public abstract class Delivery
+    {
+        public abstract string Chosen();
+        public abstract float Price();
+    }
+}
---
+++ ShopDelivery/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ShopDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delivery delivery = new DeliveryInStore();
+            Console.WriteLine($"{delivery.Chosen()} price: {delivery.Price()}" );
+
+            var deliverStandard = new DeliveryStandard(delivery);
+            Console.WriteLine($"{deliverStandard.Chosen()} price: {deliverStandard.Price()}");
+
+            var deliveryExpress = new DeliveryExpress(deliverStandard);
+            Console.WriteLine($"{deliveryExpress.Chosen()} price: {deliveryExpress.Price()}");
+
+            var deliveryExpressGiftBox = new DeliveryGiftBox(deliveryExpress);
+            Console.WriteLine($"{deliveryExpressGiftBox.Chosen()} price: {deliveryExpressGiftBox.Price()}");
+        }
+    }
+
+    public abstract class DeliveryDecorator : Delivery
+    {
+        private Delivery _delivery;
+
+        public DeliveryDecorator(Delivery delivery)
+        {
+            _delivery = delivery;
+        }
+
+        public override string Chosen() => _delivery.Chosen();
+        public override float Price() => _delivery.Price();
+    }
+    public class DeliveryStandard : DeliveryDecorator
+    {
+        public DeliveryStandard(Delivery delivery) : base(delivery)
+        {
+        }
+
+        public override string Chosen() => base.Chosen() + " Standard";
+        public override float Price() => base.Price() + 5.99f;
+    }
+
+    public class DeliveryExpress : DeliveryDecorator
+    {
+        public DeliveryExpress(Delivery delivery) : base(delivery)
+        {
+        }
+
+        public override string Chosen() => base.Chosen() + " Express";
+        public override float Price() => base.Price() + 9.99f;
+    }
+
+    public class DeliveryGiftBox : DeliveryDecorator
+    {
+        public DeliveryGiftBox(Delivery delivery) : base(delivery)
+        {
+        }
+
+        public override string Chosen() => base.Chosen() + " GiftBox";
+        public override float Price() => base.Price() + 3.99f;
+    }
+}
